944ca8870ac2985b12109d5f22d82e5df1620c5b
ShabnamBanuH <167029571+ShabnamBanuH@users.noreply.github.com>
2024-07-30 16:39:11 +0530

    Create obj.js

---
+++ javascriptBasic/obj.js
@@ -0,0 +1,7 @@
+var obj = {name:'Banu', age : 33 , phone:9876543210 , address:'Nellore' , city:'Hyderabad' ,
+          wish:function(){
+              console.log("Hello function!!")
+          }}
+console.log(Object.values(obj))
+console.log(obj.name)
+console.log(obj.wish())
